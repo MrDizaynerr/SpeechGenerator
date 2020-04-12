d8bfa29601845ff591493b98124816a822e45cfa
slavaviktorov <viktorov.slava@inbox.ru>
2020-04-12 18:45:34 +1000

    First version of speech generator. Got a plan to make BUTHURT generator.

--- buthert/generate.py
+++ buthert/generate.py
@@ -22,24 +22,17 @@ morpher = mrph.MorphAnalyzer()
 NOUN_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'NOUN']  # имя существительное	            хомяк
 ADJF_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'ADJF']  # имя прилагательное (полное)	    хороший
 ADJS_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'ADJS']  # имя прилагательное (краткое)	    хорош
-COMP_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'COMP']  # компаратив	                    лучше, получше, выше
-VERB_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'VERB']  # глагол (личная форма)	            говорю, говорит, говорил
-INFN_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'INFN']  # глагол (инфинитив)	            говорить, сказать
-PRTF_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'PRTF']  # причастие (полное)	            прочитавший, прочитанная
+COMP_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'COMP']  # компаратив	                    лучше, получше, выше
+VERB_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'VERB']  # глагол (личная форма)	        говорю, говорит, говорил
+INFN_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'INFN']  # глагол (инфинитив)	            говорить, сказать
+PRTF_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'PRTF']  # причастие (полное)	            прочитавший, прочитанная
 PRTS_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'PRTS']  # причастие (краткое)	            прочитана
-GRND_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'GRND']  # деепричастие	                    прочитав, рассказывая
-NUMR_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'NUMR']  # числительное	                    три, пятьдесят
+GRND_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'GRND']  # деепричастие	                    прочитав, рассказывая
+NUMR_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'NUMR']  # числительное	                    три, пятьдесят
 ADVB_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'ADVB']  # наречие	                        круто
 NPRO_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'NPRO']  # местоимение-существительное	    он
 PRED_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'PRED']  # предикатив	                    некогда
 PREP_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'PREP']  # предлог	                        в
 CONJ_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'CONJ']  # союз	                            и
-PRCL_bank = [x for x in word_bank if
-             morpher.parse(x)[0].tag.POS == 'PRCL']  # частица	                        бы, же, лишь
+PRCL_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'PRCL']  # частица	                        бы, же, лишь
 INTJ_bank = [x for x in word_bank if morpher.parse(x)[0].tag.POS == 'INTJ']  # междометие	                    ой
